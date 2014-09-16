da9ee631a02e4b38e0504092484941d33896c8e1
Tyler Ellis <Tyler@Tylers-MacBook-Pro.local>
2014-09-16 17:23:37 -0300

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Tyler Ellis on 9/16/14.
+//  Copyright (c) 2014 PaceMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Tyler Ellis on 9/16/14.
+//  Copyright (c) 2014 PaceMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
