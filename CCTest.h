c477f50eb2abe408e621c6d72d88cafd04307e7f
Tyler Ellis <Tyler@Tylers-MacBook-Pro.local>
2014-09-16 17:35:23 -0300

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Tyler Ellis on 9/16/14.
+//  Copyright (c) 2014 PaceMate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Tyler Ellis on 9/16/14.
+//  Copyright (c) 2014 PaceMate. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
